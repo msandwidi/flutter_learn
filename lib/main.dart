c8990c657a3f8b69f7fd94f3a96298576e1a5407
msandwidi <shoudou@outlook.com>
2019-11-19 17:10:30 -0600

    implemented listTile for short list

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,38 @@ import './home.dart';
 main(List<String> args) {
   runApp(MaterialApp(
     title: "Baobab",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+      ),
+      body: getListView(),
+    ),
   ));
+}
+
+Widget getListView() {
+  var listview = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful view"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("Landscape clicked");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("It is time"),
+        trailing: Icon(Icons.watch),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("My Number"),
+        subtitle: Text("Beautiful view"),
+      ),
+    ],
+  );
+  return listview;
 }
