6f4c60d4323213bb8633ebcc04462d9680cca835
msandwidi <shoudou@outlook.com>
2019-11-19 17:54:32 -0600

    added statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,48 @@
 import 'package:flutter/material.dart';
-import './home.dart';
 
-main(List<String> args) {
+void main() {
   runApp(MaterialApp(
-    title: "Baobab",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("My App"),
-      ),
-      body: getLongListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          debugPrint("fab clicked");
-        },
-        child: Icon(Icons.add),
-        tooltip: "Add One More Item",
-      ),
-    ),
+    title: "Statefull",
+    home: FavoriteCity(),
   ));
 }
 
-// show snackbar
-Widget showSnackBar(BuildContext context, String item) {
-  var snackbar = SnackBar(
-    content: Text("You added $item"),
-    action: SnackBarAction(
-      label: "UNDO",
-      onPressed: () {
-        debugPrint("Action pressed");
-      },
-    ),
-  );
-  Scaffold.of(context).showSnackBar(snackbar);
+// statefull widget
+class FavoriteCity extends StatefulWidget {
+  @override
+  _FavoriteCityState createState() => _FavoriteCityState();
 }
 
-// meant for a short list
-Widget getListView() {
-  var listview = ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful view"),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: () {
-          debugPrint("Landscape clicked");
-        },
+//state class
+class _FavoriteCityState extends State<FavoriteCity> {
+  String city = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Statefull"),
       ),
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("It is time"),
-        trailing: Icon(Icons.watch),
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        alignment: Alignment.center,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String input) {
+                setState(() {
+                  city = input;
+                });
+              },
+            ),
+            Text(
+              "you enterer $city",
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+            )
+          ],
+        ),
       ),
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("My Number"),
-        subtitle: Text("Beautiful view"),
-      ),
-    ],
-  );
-  return listview;
-}
-
-// generate long list of elements
-List<String> getListElement() {
-  return List<String>.generate(1000, (counter) => "counter $counter");
-}
-
-// generate long list view
-Widget getLongListView() {
-  var listItems = getListElement();
-
-  var listView = ListView.builder(
-    itemBuilder: (context, index) {
-      return ListTile(
-        title: Text(listItems[index]),
-        onTap: () => showSnackBar(context, listItems[index]),
-      );
-    },
-  );
-  return listView;
+    );
+  }
 }
