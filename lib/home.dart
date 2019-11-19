34e9a44f3b157b019fc7b52f021c3def2f3b1f04
msandwidi <shoudou@outlook.com>
2019-11-19 16:35:51 -0600

    added column and rows

--- lib/home.dart
+++ lib/home.dart
@@ -5,19 +5,38 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        alignment: Alignment.center,
-        color: Colors.lightBlue[700],
-        child: Text(
-          "Hello",
-          style: TextStyle(
-            color: Colors.white,
-            decoration: TextDecoration.none,
-            fontFamily: "Raleway",
-            fontWeight: FontWeight.w700,
-            fontStyle: FontStyle.italic,
-          ),
-        ),
-      ),
+          alignment: Alignment.center,
+          color: Colors.lightBlue[700],
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      "Hello My name is mamadou sandwidi",
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.none,
+                        fontFamily: "Raleway",
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "There",
+                    style: TextStyle(
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                      fontFamily: "Raleway",
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
